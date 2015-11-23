b77e7f982fdcf7ea55bd53f67f806fe9a9b98040
chandragithub2014 <b.chandrasaimohan@gmail.com>
2015-11-24 03:53:42 +0530

    updated with constants & icons

---
+++ MedData/mobile/src/main/java/com/android/meddata/MedDataUtils/MedDataConstants.java
@@ -0,0 +1,11 @@
+package com.android.meddata.MedDataUtils;
+
+/**
+ * Created by CHANDRASAIMOHAN on 11/24/2015.
+ */
+public class MedDataConstants {
+
+    public static final boolean USE_TEST_SERVICE = true;
+    public static final String  LOGIN_ID = "test1";
+    public static final String LOGIN_PASS_WORD = "test@123";
+}
---
+++ MedData/wear/src/main/java/com/android/meddata/MedDataUtils/MedDataConstants.java
@@ -0,0 +1,9 @@
+package com.android.meddata.MedDataUtils;
+
+/**
+ * Created by CHANDRASAIMOHAN on 11/24/2015.
+ */
+public class MedDataConstants {
+
+    public static final boolean USE_TEST_SERVICE = true;
+}
